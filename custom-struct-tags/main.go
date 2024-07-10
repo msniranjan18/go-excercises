4b4273624d8dff237747775b0a98a6b13054c5ed
Manvendra S. Niranjan <manvendraniranjan528@gmail.com>
2024-07-10 17:12:13 +0530

    custom struct tags

---
+++ custom-struct-tags/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type User struct {
+	FName string `color:"dark" default:"RAM"`
+	LName string `foo:"bar" ram:"shyam"`
+}
+
+func main() {
+	fmt.Println("Use-of-custom-struct-tags")
+	u := User{}
+
+	uType := reflect.TypeOf(u)
+
+	fnameField := uType.Field(0)
+	lnameField := uType.Field(1)
+
+	fmt.Println(fnameField.Tag.Get("color"), fnameField.Tag.Get("default"))
+	fmt.Println(lnameField.Tag.Get("foo"), lnameField.Tag.Get("ram"))
+
+}
+
+/*
+go run main.go
+
+Output:
+Use-of-custom-struct-tags
+dark RAM
+bar shyam
+*/
